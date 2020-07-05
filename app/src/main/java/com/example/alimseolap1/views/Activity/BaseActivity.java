75ae3633a609efc5a346381cc337c186bd33e759
HWRdeveloper <ruehyeon12@gmail.com>
2020-07-05 18:07:42 +0900

    상태바 하얗게 만듦

--- app/src/main/java/com/example/alimseolap1/views/Activity/BaseActivity.java
+++ app/src/main/java/com/example/alimseolap1/views/Activity/BaseActivity.java
@@ -17,7 +17,7 @@ public class BaseActivity extends AppCompatActivity {
             if (view != null) {
                 // 23 버전 이상일 때 상태바 하얀 색상에 회색 아이콘 색상을 설정
                 view.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                getWindow().setStatusBarColor(Color.parseColor("#f2f2f2"));
+                getWindow().setStatusBarColor(Color.parseColor("#ffffff"));
             }
         }else if (Build.VERSION.SDK_INT >= 21) {
             // 21 버전 이상일 때
